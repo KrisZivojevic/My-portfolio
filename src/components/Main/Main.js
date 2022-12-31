6443159b706aa6fb1d56fbe479b944f39283e243
KrisZivojevic <kristina.zivojevic@gmail.com>
2022-12-31 01:05:05 +0100

    footer section

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -1,6 +1,6 @@
 import AOS from "aos";
 import { useEffect } from "react";
-import styles from './Main.module.css';
+import styles from "./Main.module.css";
 import "aos/dist/aos.css";
 import Projects from "../Projects/Projects";
 import Contact from "../Contact/Contact";
@@ -20,7 +20,9 @@ const Main = () => {
         <article>It is a long established fact</article>
       </section>
       <section data-aos="fade-left" className={styles.main__section}>
-        <article>Lorem Ipsum is simply dummy</article>
+        <article className={styles.main__article}>
+          Lorem Ipsum is simply dummy
+        </article>
         <article>It is a long established fact</article>
       </section>
       <Projects />
