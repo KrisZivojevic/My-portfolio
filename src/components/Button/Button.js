27c6fb8f79fc2ee163acfc199504c49a883b33c2
KrisZivojevic <kristina.zivojevic@gmail.com>
2023-01-06 01:16:33 +0100

    add logo and button hover

--- src/components/Button/Button.js
+++ src/components/Button/Button.js
@@ -2,7 +2,7 @@ import styles from './Button.module.css';
 
 const Button = (props) => {
   return (
-    <button className={styles.button}>{props.name}</button>
+    <button className={`${styles.button} ${props.style}`}>{props.name}</button>
   )
 }
 
--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -1,18 +1,19 @@
 import styles from './Navigation.module.css';
 import Button from "../Button/Button";
+import profile from "../../assets/images/logo.png";
 
 const Navigation = () => {
   return (
     <nav className={styles.nav__wrapper}>
       <div className={`${styles.nav__style} max-content`}>
-        <div className="pl">K</div>
+        <div><img src={profile} alt='logo' /></div>
         <div className={`${styles.list} pr`}>
           <li>Home</li>
           <li>About</li>
           <li>Education</li>
           <li>Projects</li>
           <li>Contact</li>
-          <li><a href="../../assets/cv/KristinaZivojevicCV.pdf" download><Button name="Resume" /></a></li>
+          <li><a href="../../assets/cv/KristinaZivojevicCV.pdf" download><Button name="Resume" style={styles.nav__button} /></a></li>
         </div>
       </div>
     </nav>
