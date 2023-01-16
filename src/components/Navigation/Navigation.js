b7ce1c4927140be47087e080dba4b238f82cc26d
KrisZivojevic <kristina.zivojevic@gmail.com>
2023-01-16 19:18:54 +0100

    enhancement arrow animation, scroll to top

--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -6,34 +6,32 @@ import { HiMenuAlt3 } from "react-icons/hi";
 import { IoCloseOutline } from "react-icons/io5";
 
 const Navigation = (props) => {
-  const [id, setId] = useState("");
   const [isOpen, setIsOpen] = useState(false);
-
-  const getId = (event) => {
-    const name = event.target.getAttribute("name");
-    setId(name);
-  }
   
-  const scrollHandler = () => {
-    props.scrollHandler(id);
+  const scrollHandler = (event) => {
+    const name = event.target.getAttribute("name");
+    const element = document.getElementById(name);
+    if (element) {
+      element.scrollIntoView({ behavior: 'smooth' });
+    }
   }
 
   return (
     <nav className={styles.nav__wrapper}>
       <div className={`${styles.nav__style} max-content`}>
         <div>
-          <img src={profile} alt="logo" />
+          <img src={profile} alt="logo" name="home" onClick={scrollHandler} />
         </div>
-        <div onClick={() => setIsOpen(prev => !prev)}>
+        <div className={styles.menu__icon} onClick={() => setIsOpen(prev => !prev)}>
           {
             isOpen ? <IoCloseOutline className={styles.nav__icon} /> : <HiMenuAlt3 className={styles.nav__icon} />
           }
         </div>
-        <div className={`${styles.list} pr ${isOpen && styles.show}`}>
-          <li name="home" onMouseEnter={getId} onClick={scrollHandler}>Home</li>
-          <li name="about" onMouseEnter={getId} onClick={scrollHandler}>About</li>
-          <li name="projects" onMouseEnter={getId} onClick={scrollHandler}>Projects</li>
-          <li name="contact" onMouseEnter={getId} onClick={scrollHandler}>Contact</li>
+        <div className={`${styles.list} ${styles.small_devices} ${isOpen && styles.show}`}>
+          <li name="home" onClick={scrollHandler}>Home</li>
+          <li name="about" onClick={scrollHandler}>About</li>
+          <li name="projects" onClick={scrollHandler}>Projects</li>
+          <li name="contact" onClick={scrollHandler}>Contact</li>
           <li>
             <a href="https://drive.google.com/file/d/1aOK_ebI8hqaoEcAuW94yw6eW7uG4EHVw/view?usp=sharing">
               <Button name="Resume" style={styles.nav__button} />
